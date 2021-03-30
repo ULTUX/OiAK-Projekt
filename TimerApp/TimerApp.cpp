ab1a0788f4c80c4bc710f33490b5afda222e8f22
WNowak <bulion@zoho.com>
2021-03-30 13:20:57 +0200

    write code that skipps CPU caching, first working mem. latency measurement

--- TimerApp/TimerApp.cpp
+++ TimerApp/TimerApp.cpp
@@ -1,95 +1,131 @@
-﻿// Code for testing a small, stand-alone section of code
-// for repeatable results.
-// This code will work as is in Microsoft(R) Visual C++(R) 5.0 compiler.
-// Simply add in your own code in the marked sections and compile
-// it.
-#include <stdio.h>
+﻿// CodeTesting.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
 
-#define CPUID __asm __emit 0fh __asm __emit 0a2h
-#define RDTSC __asm __emit 0fh __asm __emit 031h
+#include <iostream>
+#include <immintrin.h>
+using namespace std;
 
-__int32 TestFunc();
+int* Array;
+//Clock frequency in Ghz
+const float BASE_CLOCK = 3.56;
 
-void main() {
-	unsigned base = 0;
-	__int32 cyc = 0;
-	// The following tests run the basic cycle counter to find
-	// the overhead associated with each cycle measurement.
-	// It is run multiple times simply because the first call
-	// to CPUID normally takes longer than subsequent calls.
-	// Typically after the second run the results are 
-	// consistent. It is run three times just to make sure.
-	__asm {
-		CPUID
-		RDTSC
-		mov cyc, eax
-		CPUID
-		RDTSC
-		sub eax, cyc
-		mov base, eax
-		CPUID
-		RDTSC
-		mov cyc, eax
-		CPUID
-		RDTSC
-		sub eax, cyc
-		mov base, eax
-		CPUID
-		RDTSC
-		mov cyc, eax
-		CPUID
-		RDTSC
-		sub eax, cyc
-		mov base, eax
-	} // End inline assembly
-	// This section calls the function that contains the user's
-	// code. It is called multiple times to eliminate instruction
-	// cache effects and get repeatable results.
-	int sum = 0;
-	int j = 0;
-	for (int i = 0; i < 200; i++) {
-		if (i >= 10) {
-			sum += TestFunc();
-			j++;
-		}
-	}
-	printf("%d\n", sum);
-	sum /= j;
-	sum -= base;
-	// By the second or third run, both data and instruction
-	// cache effects should have been eliminated, and results
-	// will be consistent.
-	printf("Base : %d\n", base);
-	printf("Cycle counts:\n");
-	printf("%d\n", sum);
+__int32 start() {
+    __int32  cycles;
+    __asm {
+        CPUID
+        RDTSC
+        mov cycles, eax
+    }
+    return cycles;
 }
 
-__int32 TestFunc() {
-	// *** REPLACE THIS SECTION WITH YOUR OWN VARIABLES
-	volatile __int32 cycles;
-	volatile __int32 
-	volatile  x;
-	volatile int i;
-	// Load the values here, not at creation, to make sure
-	// the data is moved into the cache.
-	cycles = 0;
-	i = 0;
-	x = 0;
+__int32 stop(int startCycles) {
+    __int32  cycles = startCycles;
+    __asm {
+        CPUID
+        RDTSC
+        sub eax, cycles
+        mov cycles, eax
+    }
+}
 
-	// *** END OF USER SECTION
-	__asm {
-		CPUID
-		RDTSC
-		mov cycles, eax
-	}
-	for (i = 0; i < 100; i++) {
-		x++;
-	}
-	__asm {
-		CPUID
-		RDTSC
-		sub eax, cycles
-		mov cycles, eax
-	}
-	return cycles;
+void initializeWithoutCache() {
+    Array = new int[800];
+    while ((int)Array % 16 != 0) {
+        delete[800] Array;
+        Array = new int[800];
+    }
+    //Load data to mem
+    for (int i = 0; i < 200; i++) {
+        __m128i vals = _mm_setr_epi32(4 * i + 0, 4 * i + 1, 4 * i + 2, 4 * i + 3);
+        _mm_stream_si128((__m128i*)Array + i, vals);
+    }
+}
+
+void initializeWithCache() {
+    Array = new int[800];
+    while ((int)Array % 16 != 0) {
+        delete[800] Array;
+        Array = new int[800];
+    }
+    for (int i = 0; i < 800; i++) {
+        Array[i] = i;
+        if (i % 4 == 0 && i > 0) {
+            __m128i testVal = _mm_loadu_si128(((__m128i*)Array) + (i - 4) / 4);
+        }
+    }
+}
+
+__int32 testWithoutCache() {
+    volatile __int32 cycles;
+    volatile int i = 0;
+    __m128i data;
+
+    cycles = start();
+
+    for (int i = 0; i < 200; i++) {
+        __m128i data = _mm_stream_load_si128((__m128i*)(Array) + i);
+    }
+
+    cycles = stop(cycles);
+    return cycles;
+}
+volatile __int32 testWithCache() {
+    volatile __int32 cycles;
+    volatile int i = 0;
+    __m128i data;
+    cycles = start();
+
+    for (; i < 1; i++) {
+        data = _mm_loadu_si128((__m128i*)(Array) + i);
+    }
+
+    cycles = stop(cycles);
+    return cycles;
+}
+
+int main()
+{
+    //initializeWithCache();
+    initializeWithoutCache();
+    unsigned base = 0;
+    __int32 cyc = 0;
+    //Cache warming for timer func
+    __asm {
+        CPUID
+        RDTSC
+        mov cyc, eax
+        CPUID
+        RDTSC
+        sub eax, cyc
+        mov base, eax
+        CPUID
+        RDTSC
+        mov cyc, eax
+        CPUID
+        RDTSC
+        sub eax, cyc
+        mov base, eax
+        CPUID
+        RDTSC
+        mov cyc, eax
+        CPUID
+        RDTSC
+        sub eax, cyc
+        mov base, eax
+    }
+    int latency = 0;
+    int j = 0;
+    for (int i = 0; i < 200; i++) {
+        if (i >= 10) {
+            //latency = testWithCache();
+            latency = testWithoutCache();
+            j++;
+        }
+    }
+    printf("%d\n", latency);
+    latency -= base;
+    printf("Latency:.............%d clock cycles (%f ns)", latency, latency / BASE_CLOCK);
+
+
 }
