8e8e0f9b75c89bd8580c6aada28b89c1d4a7a2da
WNowak <bulion@zoho.com>
2021-04-13 17:50:14 +0200

    change default settings

--- final_version/src/main.cpp
+++ final_version/src/main.cpp
@@ -8,10 +8,10 @@ int main()
     int count = 100;
     int startSize = 800;
     int increment = 0;
-    double multiplier = 1.1;
+    double multiplier = 1.15;
     int *cacheTest = testMultiple(startSize, increment, multiplier, count, true);
     int *noCacheTest = testMultiple(startSize, increment, multiplier, count, false);
 
-    CSVWriter(3.56, cacheTest, noCacheTest, count, startSize, increment, multiplier);
+    CSVWriter(2.3, cacheTest, noCacheTest, count, startSize, increment, multiplier);
     return 0;
 }
